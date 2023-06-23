96821314b806bc04de54656cdb5c449ccb21db9a
Julián Lionti <julian.lionti@teaminternational.com>
2023-06-23 12:32:47 -0300

    Testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
+import manifest from "./manifest.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [react(), VitePWA({ manifest })],
 });
